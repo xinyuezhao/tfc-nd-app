ce0266dd0ce34e8ec78956d6ca34e14d5409ba53
cindy zhao <xinyuezhao96@gmail.com>
2022-01-26 08:15:45 -0800

    add testsuite

--- cmd/testsuite/urls_test.go
+++ cmd/testsuite/urls_test.go
@@ -1,7 +1,7 @@
 package main
 
 const (
-	credentialsmgrBaseURL   = "https://resourcemgr.kubese.svc/credentialsmgr/"
+	credentialsmgrBaseURL   = "https://172.31.187.83/sedgeapi/v1/cisco-terraform/credentialsmgr/"
 	credentialsURL          = credentialsmgrBaseURL + "api/terraform.argo.cisco.com/v1/credentials"
 	credentialsTerraformURL = credentialsURL + "/terraform"
 )
