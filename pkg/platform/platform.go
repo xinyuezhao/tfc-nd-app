7ece07169179bde5045a7bd63ec0b8d9133f73bf
cindy zhao <xinyuezhao96@gmail.com>
2022-01-28 06:30:20 -0800

    Revert "remove unused intersight/kind & update app.json"
    This reverts commit d000a8636d677377221fc69b6ef0aadf5192971b.

--- pkg/platform/platform.go
+++ pkg/platform/platform.go
@@ -1,15 +1,21 @@
 package platform
 
 import (
+	"golang.cisco.com/argo-galaxies/intersight"
 	"golang.cisco.com/argo-galaxies/nexusdashboard"
 	"golang.cisco.com/argo/pkg/config"
 	"golang.cisco.com/argo/pkg/platform"
+	"golang.cisco.com/argo/pkg/platformenv/kind"
 )
 
 func New(platformName platform.Name, appName string, cs config.Store) platform.Infra {
 	switch platformName {
+	case platform.Kind:
+		return kind.New(appName, cs)
 	case platform.NexusDashboard:
 		return nexusdashboard.New(appName, cs)
+	case platform.Intersight:
+		return intersight.New(appName, cs)
 	}
 	return nil
 }
