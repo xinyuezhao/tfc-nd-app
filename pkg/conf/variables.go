27f61c86c3f2c6062130fef8a61bc34da5352052
cindy zhao <xinyuezhao96@gmail.com>
2022-03-02 10:39:05 -0800

    include organization usage report in org module

--- pkg/conf/variables.go
+++ pkg/conf/variables.go
@@ -1,14 +1,16 @@
 package conf
 
 const (
-	Vendor           = "cisco"
-	Version          = "0.0.2"
-	App              = "terraform"
-	FeatureName      = "tfc-agent"
-	AgentPoolURL     = "https://app.terraform.io/api/v2/agent-pools"
-	FeatureURL       = "https://resourcemgr.kubese.svc/api/config/dn/appinstances/cisco-terraform"
-	FeatureDelURL    = "https://resourcemgr.kubese.svc/api/config/delfeatureinstance"
-	AgentURL         = "https://app.terraform.io/api/v2/agents"
-	CredentialsURL   = "https://securitymgr-svc.securitymgr.svc:8989/api/config/getcredentials"
-	FeatureCreateURL = "https://resourcemgr.kubese.svc/api/config/createfeatureinstance"
+	Vendor            = "cisco"
+	Version           = "0.0.2"
+	App               = "terraform"
+	FeatureName       = "tfc-agent"
+	AgentPoolURL      = "https://app.terraform.io/api/v2/agent-pools"
+	FeatureURL        = "https://resourcemgr.kubese.svc/api/config/dn/appinstances/cisco-terraform"
+	FeatureDelURL     = "https://resourcemgr.kubese.svc/api/config/delfeatureinstance"
+	AgentURL          = "https://app.terraform.io/api/v2/agents"
+	CredentialsURL    = "https://securitymgr-svc.securitymgr.svc:8989/api/config/getcredentials"
+	FeatureCreateURL  = "https://resourcemgr.kubese.svc/api/config/createfeatureinstance"
+	CredentialsAddURL = "https://securitymgr-svc.securitymgr.svc:8989/api/config/addcredentials"
+	OrgURL            = "https://app.terraform.io/api/v2/organizations"
 )
