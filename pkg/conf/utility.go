31550b12a3e639e2922e828c631082e2e0ce3465
cindy zhao <xinyuezhao96@gmail.com>
2022-05-06 17:59:45 -0700

    [bugfix] remove unused commented line for error msg

--- pkg/conf/utility.go
+++ pkg/conf/utility.go
@@ -532,13 +532,11 @@ func NewOrganization(org *tfe.Organization, newOrg terraformv1.Organization) err
 	errors := make([]error, 0)
 	usage, errUsage := QueryOrgUsage(org)
 	if errUsage != nil {
-		// errors = append(errors, errUsage)
 		err := fmt.Errorf("error from QueryOrgUsage in func NewOrganization")
 		return core.NewError(err, errUsage)
 	}
 	subscription, errSub := QueryOrgSubscription(org)
 	if errSub != nil {
-		// errors = append(errors, errSub)
 		err := fmt.Errorf("error from QueryOrgSubscription in func NewOrganization")
 		return core.NewError(err, errSub)
 	}
